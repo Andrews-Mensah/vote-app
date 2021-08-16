f4b3b2fb03fbf43fad42b60dd5268be7fec5d9ad
Andrews Mensah <andrews.mensah@amalitech.org>
2021-08-16 09:20:33 +0000

    express-ejs-layout installred

--- models/votes.js
+++ models/votes.js
@@ -2,22 +2,22 @@ const pg = require('pg');
 const Schema = pg.Schema;
 
 
-const voteSchema = new Schema({
-    presidential: {
-        type: String,
-        required: true
-    },
+// const voteSchema = new Schema({
+//     presidential: {
+//         type: String,
+//         required: true
+//     },
 
-    points: {
-        type: String,
-        required: true
-    }
+//     points: {
+//         type: String,
+//         required: true
+//     }
 
-});
+// });
 
 
 //create collection and add schema
-const Vote = pg.model('votes', voteSchema);
+// const Vote = pg.model('votes', voteSchema);
 
 
 //exporting votes
--- routes/poll.js
+++ routes/poll.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const pg = require('pg');
 
-const Vote = require('../models/votes');
+// const Vote = require('../models/votes');
 
 
 
